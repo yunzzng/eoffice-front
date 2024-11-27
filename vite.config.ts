c1f6696c993fa5692b7ddbe3cc1d7fe37ea8b386
yunzzng <leejiyun0114@naver.com>
2024-11-27 10:33:46 +0900

    oauth 완료

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // /api/oauth 경로는 그대로 유지
+      "/api/oauth": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path, 
+      },
+      // /api 경로는 rewrite 적용
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
