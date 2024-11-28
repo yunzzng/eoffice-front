b1f829d15eefb6d71db5d98a54b7ee98833cf7eb
yunzzng <leejiyun0114@naver.com>
2024-11-28 10:01:53 +0900

    splash추가

---
+++ src/components/splash/splash.tsx
@@ -0,0 +1,30 @@
+import styles from './Splash.module.css';
+import computerImage from '../../../public/img/computerImage.png';
+import { useNavigate } from 'react-router-dom';
+
+const Splsah = () => {
+    const navigate = useNavigate();
+
+    const navigateToLogin = () => {
+        navigate("/login");
+    };
+    const navigateToSignup = () => {
+        navigate("/signup");
+    };
+
+    return (
+        <div className={styles.header}>
+          <div className={styles.logo} >E-Office</div>
+          <div className={styles.content}>
+            <h1 className={styles.text}>업무의 모든 것을 <br />하나의 플랫폼에서</h1>
+            <div className={styles.buttons}>
+              <button onClick={navigateToLogin} className={styles.loginButton}>로그인</button>
+              <button onClick={navigateToSignup} className={styles.signupButton}>회원가입</button>
+            </div>
+          </div>
+          <img src={computerImage} alt="컴퓨터" className={styles.image} />
+        </div>
+      );
+};
+
+export default Splsah;
