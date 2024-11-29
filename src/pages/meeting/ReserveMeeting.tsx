d0a86bc55c77825151693d82c52d8353fc05965e
ahyeon <4641090@naver.com>
2024-11-29 22:21:05 +0900

    병합충돌 수정

--- src/pages/meeting/ReserveMeeting.tsx
+++ src/pages/meeting/ReserveMeeting.tsx
@@ -7,15 +7,15 @@ import { useNavigate } from "react-router-dom";
 const ReserveMeeting = () => {
     const navigate = useNavigate();
 
-    const reserve = () => {
-        navigate('/')
-    }
+    const handlereserve = () => {
+        navigate('/');
+    };
+
     return (
         <>
-        <Header />
-        <Sidebar />
-        <Footer />
-        <NavigateButtons label="회의실 예약하기" onClick={} />
+            <Header />
+            <Sidebar />
+            <Footer />
         </>
     )
 }
