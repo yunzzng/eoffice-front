5ac9f14ec7b1f15b0e8e0e7a3a2e0d8a9eae8edd
yunzzng <leejiyun0114@naver.com>
2024-11-29 23:14:23 +0900

    헤더 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import styles from "./Header.module.css";
 import { useNavigate } from "react-router-dom";
 import profileimg from "../../../public/images/ix_user-profile-filled.png";
+import guestprofile from "../../../public/images/guest_profile.png";
 
 interface User {
     _id: string;
