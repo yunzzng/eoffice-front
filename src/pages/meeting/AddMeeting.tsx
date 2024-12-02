f7dfe9166354f557cae37f8b67ca58430dc13311
yunzzng <leejiyun0114@naver.com>
2024-12-02 14:00:19 +0900

    코드리뷰 수정사항 반영

--- src/pages/meeting/AddMeeting.tsx
+++ src/pages/meeting/AddMeeting.tsx
@@ -1,52 +1,55 @@
-
 import Sidebar from "../../components/sidebar/Siderbar";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
-import { NavigateButtons } from "../../context/ButtonContext";
+import { NavigateButtons } from "../../components/button/Button";
 import styles from "../../css/meetingStyles/AddMeeting.module.css";
 import { ChangeEvent, useState } from "react";
-import Rectangle from '../../../public/images/Rectangle 27.png';
 import { useNavigate } from "react-router-dom";
+import { ImageUpload } from "../../context/ImgUploadContext";
 
 const AddMeeting = () => {
     const navigate = useNavigate();
-    const [formData, setFormData] = useState({
+    const [inputValue, setInputValue] = useState({
         name: "",
         location:"",
         person: ""
-    });
+    }); //회의실 이름,위치,인원 
 
-    const [inputFile, setInputFile] = useState<File>();
+    const [inputFile, setInputFile] = useState<File>(); //회의실 이미지
 
 
     //이름,위치,인원 input Change
     const handleInputChange = (e:ChangeEvent<HTMLInputElement>) => {
         const {name, value} = e.target;
-        setFormData({...formData, [name] : value});
+        setInputValue({...inputValue, [name] : value});
     }
 
-    //이미지 업로드
-    const handleImageUpload = (e:ChangeEvent<HTMLInputElement>) => {
-        if(!e.target.files) {
-            return;
-        }
-
-        if(e.target.files[0]) {
-            setInputFile(e.target.files[0]);
-        }
-    }
 
     //회의실 등록하기 버튼
     const handleSubmit = async() => {
-        try{
-            const response = await fetch('/api', {
-                method: "POST"
+        const formData = new FormData(); //서버로 전송 할 객체 생성
 
-            });
-        }catch(err) {
-            console.log('데이터 저장 실패', err)
+        formData.append("name", inputValue.name);
+        formData.append("location", inputValue.location);
+        formData.append("person", inputValue.person);
+        if(inputFile) {
+            formData.append("file", inputFile);
         }
 
+        try{
+            const response = await fetch('/api/meeting/meetingrooms', {
+                method: "POST",
+                body: formData,
+            });
+            if(response.ok) {
+                console.log('데이터 저장 성공');
+            }else{
+                console.log('api요청 실패');
+                return;
+            }
+        }catch(err) {
+            console.log('데이터 저장 실패', err);
+        }
         navigate('/meetinglist');
     }
 
@@ -55,20 +58,8 @@ const AddMeeting = () => {
         <Header />
         <Sidebar />
         <Footer />
-            <div className={styles.addmeeting_box}>
-                <label htmlFor="Image">
-                    <img
-                        className={styles.addmeeting_default_img}
-                        src={Rectangle}
-                        alt="프로필 이미지"
-                        />
-                </label>
-                <input
-                    className={styles.addmeeting_input_file}
-                    type="file"
-                    name="profileImage"
-                    id="Image"
-                    />
+            <div className={styles.addmeeting_total_box}>
+                <ImageUpload setUploadImg={setInputFile} />
                 <div className={styles.addmeeting_input_box}>
                     <div className={styles.addmeeting_input_name_box}>
                         <label className={styles.addmeeting_input_label}>이름</label>
@@ -82,9 +73,9 @@ const AddMeeting = () => {
                         <label className={styles.addmeeting_input_label}>인원</label>
                         <input className={styles.addmeeting_input}/>
                     </div>
+                    <NavigateButtons label="회의실 등록하기" onClick={handleSubmit}/>
                 </div>
             </div>
-        <NavigateButtons label="회의실 등록하기" onClick={handleSubmit}/>
         </div> 
     )
 }
