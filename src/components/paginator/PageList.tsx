b04a0a16e6ea1cb3adf0da4c0cc3ac6705832f24
React2 <pursuit0819@naver.com>
2024-12-06 22:10:21 +0900

    12월 6일 develop 가져옴

--- src/components/paginator/PageList.tsx
+++ src/components/paginator/PageList.tsx
@@ -19,10 +19,16 @@ const PageList = () => {
         <div className={styles.minutes_list_wrap} key={`minutes-key-${index}`}>
           <div className={styles.minutes_unit}>
             <ul className={styles.minutes_info}>
-              <li className={styles.minutes_title}>{minute.title}</li>
-              <li className={styles.minutes_date}>{minute.date}</li>
-              <li className={styles.minutes_attendee}>{minute.attendee}</li>
-              <li className={styles.minutes_createAt}>{minute.createAt}</li>
+              <li className={styles.minutes_title}>
+                회의록 제목: {minute.title}
+              </li>
+              <li className={styles.minutes_date}>회의 날짜: {minute.date}</li>
+              <li className={styles.minutes_attendee}>
+                회의 참석자 수 :{minute.attendees}
+              </li>
+              <li className={styles.minutes_createAt}>
+                작성 날짜: {minute.createdAt}
+              </li>
             </ul>
 
             <div className={styles.minutes_content}>{minute.content}</div>
